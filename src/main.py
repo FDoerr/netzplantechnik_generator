ce936fc9d54e7789c2dc1ce4570311013541f77e
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-11-29 15:18:05 +0100

    added tests for creating nodes

--- src/main.py
+++ src/main.py
@@ -4,8 +4,11 @@ from netzplanknoten import Netzplanknoten as knoten
 
 def main():
     knoten_1 = knoten('knoten_1', 1, ['knoten_2', 'knoten_3'])
-    print( knoten_1)
+    knoten_2 = knoten('knoten_2', 2, ['knoten_4'])
+    knoten_3 = knoten('knoten_3', 3, ['knoten_4'])
+    knoten_4 = knoten('knoten_4', 3, None)
     ...
+
 
 if __name__=='__main__':
     main()
